f77ac0f925df17c83a5fe0fa47cd1461db7217e0
friz() <frizadiga@gmail.com>
2025-06-03 18:50:21 +0700

    feat: add support for Mistral vendor in LLM integration (#2)
    * feat: add support for Mistral vendor in LLM integration
    
    - Add brand new Mistral Module.
    - Integrate Mistral as a new vendor in the Makefile.
    - Implement prompt handling for Mistral in main.go.
    
    * chore: remove unused vendor variable and debug comments from main.go
    
    * docs: add comment to clarify payload processing in ModMistralStream function

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 )
 
-// read shell env __LLM_MAIN_ENTRY_VENDOR
 var vendor string = os.Getenv("__LLM_MAIN_ENTRY_VENDOR")
 
 func main() {
@@ -15,10 +14,6 @@ func main() {
 	if os.Getenv("LLM_RES_ONLY") != "1" {
 		fmt.Println(mockRole)
 	}
-
-	// @NOTE: keep it here for debugging
-	// fmt.Println("[DEBUG] vendor:", vendor)
-	// fmt.Println("[DEBUG] prompt:", prompt)
 
 	if vendor == "openai" {
 		ModOpenAI(&prompt)
@@ -39,6 +34,10 @@ func main() {
 	if vendor == "google" {
 		ModGoogle(&prompt)
 	}
+
+	if vendor == "mistral" {
+		ModMistral(&prompt)
+	}
 }
 
 func getPromptFlag() string {
@@ -52,22 +51,3 @@ func getPromptFlag() string {
 
 	return *promptFlag
 }
-
-// func getPrompt() string {
-// 	if len(os.Args) < 2 {
-// 		fmt.Println("Error: Please provide a prompt")
-// 		os.Exit(1)
-// 	}
-//
-// 	var prompt string = os.Args[1]
-// 	// todo something like "$*" in bash
-// 	// var prompt string = strings.Join(os.Args[1:], " ")
-//
-// 	// @NOTE: keep it here for debugging
-// 	// print all args
-// 	// for i, arg := range os.Args {
-// 	// 	fmt.Println(i, arg)
-// 	// }
-//
-// 	return prompt
-// }
