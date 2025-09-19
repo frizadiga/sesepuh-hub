74b3552a5418873bd6d8f44f65f52baafec1fb53
frizadiga <frizadiga@gmail.com>
2025-09-19 16:34:41 +0700

    Fix: Correctly select model with yq and fzf
    This commit fixes an issue where the `select_model.sh`
    script was not correctly selecting the model and vendor
    from the `enums.yml` file.
    
    The following changes were made:
    
    - Use `-r` flag for `yq` to output raw string
    - Specify the correct path to `enums.yml` for `model_data`
    - Output variable assignemnts for debugging

--- select_model.sh
+++ select_model.sh
@@ -9,10 +9,10 @@ __self_path_file=$(readlink -f "$0")
 __self_path_dir=$(dirname "${__self_path_file}")
 
 fn_select_model() {
-  local model_name=$(yq '.models[].name' "${__self_path_dir}/enums.yml" | fzf) || exit 1
+  local model_name=$(yq -r '.models[].name' "${__self_path_dir}/enums.yml" | fzf) || exit 1
 
   # query once
-  model_data=$(yq '.models[] | select(.name == "'"${model_name}"'")' ./enums.yml)
+  model_data=$(yq '.models[] | select(.name == "'"${model_name}"'")' "${__self_path_dir}/enums.yml")
 
   if [ -z "${model_data}" ]; then
     echo "Error: Model '${model_name}' not found in enums.yml" >&2
@@ -28,8 +28,8 @@ fn_select_model() {
   local model=$(echo "${model_data}" | yq '.name')
   local vendor=$(echo "${model_data}" | yq '.vendor')
 
-  echo "SESEPUH_HUB_MODEL: ${model}" >&2
-  echo "SESEPUH_HUB_VENDOR: ${vendor}" >&2
+  echo "SESEPUH_HUB_MODEL=${model}" >&2
+  echo "SESEPUH_HUB_VENDOR=${vendor}" >&2
 
   export SESEPUH_HUB_MODEL="${model}"
   export SESEPUH_HUB_VENDOR="${vendor}"
